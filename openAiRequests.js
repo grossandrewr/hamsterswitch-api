ca87eacaa89a78c4f9eb92c7e159205dbf900568
Andrew Gross <gross.andrewr@gmail.com>
2025-02-02 16:59:19 -0500

    refactor openAi request file

---
+++ openAiRequests.js
@@ -0,0 +1,23 @@
+import { config } from 'dotenv';
+import OpenAI from "openai";
+
+config(); 
+const openAIKey = process.env.OPENAI_API_KEY;
+const openai = new OpenAI({ apiKey: openAIKey });
+
+export const makeGptSearchRequest = async (gptSystemPrompt, searchString) => {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: gptSystemPrompt
+      },
+      {
+        role: "user",
+        content: searchString
+      },
+    ],
+    model: "gpt-4o-2024-08-06",
+  });
+  return completion.choices[0];
+}
