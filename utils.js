c7bc7acf248422f836461c4951390de0a79eff2e
Andrew Gross <gross.andrewr@gmail.com>
2025-02-07 08:29:39 -0500

    add script to package.json

--- utils.js
+++ utils.js
@@ -1,24 +1,24 @@
-export const validateAlbumsList = content => {
+export const validateAlbumsList = (content) => {
   let contentArray;
   try {
     contentArray = JSON.parse(content);
-    contentArray = ['asfads']
+    contentArray = ["asfads"];
   } catch {
     throw new Error("Error parsing JSON");
   }
 
   if (!Array.isArray(contentArray)) {
     throw new Error("Content is not an array");
-  } 
+  }
   if (!contentArray.length) {
     throw new Error("No albums returned");
-  } 
+  }
   for (const item of contentArray) {
-    if (typeof item !== 'object' || !('album' in item) || !('artist' in item)) {
+    if (typeof item !== "object" || !("album" in item) || !("artist" in item)) {
       throw new Error("Invalid content format");
-    } 
-    if (typeof item.album !== 'string' || typeof item.artist !== 'string') {
+    }
+    if (typeof item.album !== "string" || typeof item.artist !== "string") {
       throw new Error("Invalid types for album or artist");
     }
   }
-}
+};
