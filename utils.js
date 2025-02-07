e20bd9ceb7975394747fa8dddfe21236e9391d5a
Andrew Gross <gross.andrewr@gmail.com>
2025-02-06 20:32:01 -0500

    address eslint issues

--- utils.js
+++ utils.js
@@ -2,15 +2,19 @@ export const validateAlbumsList = content => {
   let contentArray;
   try {
     contentArray = JSON.parse(content);
-  } catch (error) {
+    contentArray = ['asfads']
+  } catch {
     throw new Error("Error parsing JSON");
   }
 
   if (!Array.isArray(contentArray)) {
     throw new Error("Content is not an array");
   } 
+  if (!contentArray.length) {
+    throw new Error("No albums returned");
+  } 
   for (const item of contentArray) {
-    if (typeof item !== 'object' || !item.hasOwnProperty('album') || !item.hasOwnProperty('artist')) {
+    if (typeof item !== 'object' || !('album' in item) || !('artist' in item)) {
       throw new Error("Invalid content format");
     } 
     if (typeof item.album !== 'string' || typeof item.artist !== 'string') {
